092c141b6ab81bf76c4f67e301352d8dfad3e766
adexcel171 <godwillokenyi@gmail.com>
2024-07-17 13:47:37 +0100

    commit changes

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -10,7 +10,7 @@ const Message = ({ variant, children }) => {
     }
   };
 
-  return <div className={`p-4 rounded ${getVariantClass()}`}>{children}</div>;
+  return <div className={`p-4 sm:mt-5 rounded ${getVariantClass()}`}>{children}</div>;
 };
 
 export default Message;
--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -54,7 +54,7 @@ const Register = () => {
   
       <form onSubmit={submitHandler} className="container w-full md:w-96">
         <div className="my-2">
-          <label htmlFor="name" className="block text-sm font-medium text-white">
+          <label htmlFor="name" className="block text-sm font-medium text-black">
             Name
           </label>
           <input
@@ -68,7 +68,7 @@ const Register = () => {
         </div>
   
         <div className="my-2">
-          <label htmlFor="email" className="block text-sm font-medium text-white">
+          <label htmlFor="email" className="block text-sm font-medium text-black">
             Email Address
           </label>
           <input
@@ -84,7 +84,7 @@ const Register = () => {
         <div className="my-2">
           <label
             htmlFor="password"
-            className="block text-sm font-medium text-white"
+            className="block text-sm font-medium text-black"
           >
             Password
           </label>
@@ -101,7 +101,7 @@ const Register = () => {
         <div className="my-2">
           <label
             htmlFor="confirmPassword"
-            className="block text-sm font-medium text-white"
+            className="block text-sm font-medium text-black"
           >
             Confirm Password
           </label>
@@ -118,7 +118,7 @@ const Register = () => {
         <button
           disabled={isLoading}
           type="submit"
-          className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer my-1"
+          className="bg-blue-500 text-black px-4 py-2 rounded cursor-pointer my-1"
         >
           {isLoading ? "Registering..." : "Register"}
         </button>
@@ -127,7 +127,7 @@ const Register = () => {
       </form>
   
       <div className="mt-4">
-        <p className="text-white">
+        <p className="text-black">
           Already have an account?{" "}
           <Link
             to={redirect ? `/login?redirect=${redirect}` : "/login"}
@@ -138,11 +138,11 @@ const Register = () => {
         </p>
       </div>
     </div>
-    <img
+    {/* <img
       src="https://images.unsplash.com/photo-1576502200916-3808e07386a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2065&q=80"
       alt=""
       className="h-64 md:h-96 w-full md:w-2/3 lg:w-3/5 xl:w-2/3 rounded-lg"
-    />
+    /> */}
   </section>
   
   );
