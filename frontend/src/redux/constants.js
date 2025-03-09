d8d944118f493d95a761b208d6513ea75a4f8a3d
adexcel171 <godwillokenyi@gmail.com>
2025-03-09 08:14:29 +0100

    update

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -2,4 +2,5 @@ export const BASE_URL = "https://management-api-wyce.onrender.com";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const USERDATA_URL = "/api/userdata";
+export const SUBSCRIPTION_URL = "/api/subscriptions";
 export const UPLOAD_URL = "/api/upload";
