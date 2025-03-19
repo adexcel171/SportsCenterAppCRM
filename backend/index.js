1ca5ec7b3cbbb01b4f356ea07b5e132ebc34d293
adexcel171 <godwillokenyi@gmail.com>
2025-03-19 13:27:37 +0100

    updated inde

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://excel-crm.onrender.com",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // enable cookies
     optionsSuccessStatus: 204,
